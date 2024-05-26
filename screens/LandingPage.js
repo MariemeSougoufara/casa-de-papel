adab6daa2d58d30b383bee09aed1026585b9c03d
issakha <diouf8987@gmail.com>
2024-05-26 03:32:50 +0100

    Configuration de la navigation

---
+++ screens/LandingPage.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+
+const LandingPage = () => {
+    return (
+       <SafeAreaView>
+        <View>
+            <Text>Hello from Landing Page</Text>
+        </View>
+       </SafeAreaView>
+    );
+}
+
+const style = StyleSheet.create({
+    
+});
+
+export default LandingPage
